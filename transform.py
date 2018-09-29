d97ab54c6c578e5e1a282f1486e09b3aeb85ccb5
Bayani Julian <bayanijulian@optimus.local>
2018-09-28 19:18:50 -0500

    implemented transform, imported MP1 code

--- transform.py
+++ transform.py
@@ -34,5 +34,46 @@ def transformToMaze(arm, goals, obstacles, window, granularity):
             Maze: the maze instance generated based on input arguments.
 
     """
+    alphaLimits, betaLimits = arm.getArmLimit()
+    alphaMin, alphaMax = alphaLimits
+    betaMin, betaMax = betaLimits
+
+    rows = int(((alphaMax - alphaMin) / granularity) + 1)
+    cols = int(((betaMax - betaMin) / granularity) + 1)
+    print("rows:{}, cols:{}".format(rows, cols))
+
+    alpha = alphaMin - granularity
     
-    pass
+    startAngles = arm.getArmAngle()
+    
+    map = []
+
+    for x in range(rows):
+        row = []
+        alpha += granularity
+        beta = betaMin - granularity
+        for y in range(cols):
+            beta += granularity
+            arm.setArmAngle((alpha, beta))
+            armPos = arm.getArmPos()
+
+            isWall = doesArmTouchObstacles(armPos, obstacles) or not isArmWithinWindow(armPos, window)
+            if isWall:
+                row.append(WALL_CHAR)
+                continue
+
+            isObjective = doesArmTouchObstacles(armPos, goals)
+            if isObjective:
+                row.append(OBJECTIVE_CHAR)
+                continue
+                
+            row.append(SPACE_CHAR)
+        map.append(row)
+    
+    
+    startIndexes = angleToIdx(startAngles, (alphaMin, betaMin), granularity)
+    startAlpha, startBeta = startIndexes
+    map[startAlpha][startBeta] = START_CHAR
+    maze = Maze(map, (alphaMin, betaMin), granularity)
+    
+    return maze
